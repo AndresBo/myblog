4f7058cd5be60071abb598f5c576d11e27b72f81
AndresBo <anndresfborrero@gmail.com>
2023-03-25 22:02:03 +1000

    to route '/login' added ability to handle POST  data with form.validate_on_submit

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -24,7 +24,13 @@ def index():
     return render_template('index.html', title='home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # if all fields validate 
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+
+        return redirect('/index')
+    
     return render_template('login.html', title='Sign In', form=form)
